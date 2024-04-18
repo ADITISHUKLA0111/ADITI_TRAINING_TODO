82353c64cb0a72ff7d285935b816dec714a470e2
Aditi shukla <56030296+ADITISHUKLA0111@users.noreply.github.com>
2024-04-18 17:09:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from "react-redux";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
+import store from "./store/index.js";
+import {Provider} from 'react-redux';
 //BrowserRouter is essentially a react context
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-   
+  <Provider store={store}>
   <BrowserRouter>
     <App />
     </BrowserRouter>
-  
+    </Provider>
  
 );
 
