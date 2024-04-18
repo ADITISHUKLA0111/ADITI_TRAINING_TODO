89a13f3dca1b26e96c72f6f3ebd74fa7fa6c7df4
Aditi shukla <56030296+ADITISHUKLA0111@users.noreply.github.com>
2024-04-18 17:11:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { Provider } from "react-redux";
-import store from "./store/index.js";
+
 import Todo from "./Components/Todo";
 import Display from "./Components/Display";
 import Header from "./Components/Header.js";
 import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (<>
-    <Provider store={store}>
+  
       <Header />
       <Routes>
         <Route path="/" element={<Todo />} />
         <Route path="/lists" element={<Display />} />
+         
       </Routes>
-    </Provider>
+   
     </>
   );
 };
